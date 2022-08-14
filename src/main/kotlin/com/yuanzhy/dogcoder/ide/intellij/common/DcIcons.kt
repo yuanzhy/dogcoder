4c0ae00f30bcf3cfcdcf1fefba07db217473695e
yuanzhy <529164645@qq.com>
2022-08-14 16:22:11 +0800

    添加插件图标

--- src/main/kotlin/com/yuanzhy/dogcoder/ide/intellij/common/DcIcons.kt
+++ src/main/kotlin/com/yuanzhy/dogcoder/ide/intellij/common/DcIcons.kt
@@ -5,7 +5,7 @@ import com.intellij.openapi.util.IconLoader
 class DcIcons {
 
     companion object {
-        val LOGO = IconLoader.getIcon("/META-INF/logo.svg", DcIcons::class.java)
+//        val LOGO = IconLoader.getIcon("/META-INF/logo.svg", DcIcons::class.java)
         val LOGO_S = IconLoader.getIcon("/META-INF/logo-s.svg", DcIcons::class.java)
     }
 }
